2e8f06c0983a8ee3c9efd9cf398082f547fb3f1f
Scott Walters <srwalte2@illinois.edu>
2014-11-27 16:23:08 -0600

    Notifications toggle on row click

--- www/js/controllers/settings.js
+++ www/js/controllers/settings.js
@@ -2,16 +2,23 @@
 	'use strict';
 
 	angular.module('settings', ['http-request']).controller('SettingsController', function($scope, $settings) {
+		// initialize settings to values from server
 		$settings.get(function(err, data){
 			if (err){
 				// TODO
 				console.log(err);
 				return;
 			}
-			$scope.settings = data;
+
+			// remove loading icon
 			var loadingIcon = document.getElementById('settings-get-loading');
 			angular.element(loadingIcon).remove();
-			$scope.$apply(); // push changes to UI
+
+			// push changes to UI
+			$scope.settings = data;
+			$scope.$apply();
 		});
+
+
 	});
 })();
