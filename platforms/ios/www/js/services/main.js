9c0452788ba28136b326985bc1783c2595ad2787
Scott Walters <me@scowalt.com>
2014-12-25 18:33:43 -0800

    Built iOS for twitter changes

--- platforms/ios/www/js/services/main.js
+++ platforms/ios/www/js/services/main.js
@@ -1,6 +1,47 @@
 (function(){
 	'use strict';
-	angular.module('main').factory('$data', function() {
+
+	angular.module('main').service('TwitterService', function(){
+		var twit = new Codebird();
+		twit.setConsumerKey(secrets.twitter.consumerKey, secrets.twitter.consumerSecret);
+
+		// set up application-only auth
+		if(!localStorage.twitterBearerToken ||
+			localStorage.twitterBearerToken === 'undefined'){
+			twit.__call('oauth2_token', {}, function(reply){
+				if (!reply){
+					console.error('oauth2_token not found');
+				} else {
+					localStorage.twitterBearerToken = reply.access_token;
+					twit.setBearerToken(reply.access_token);
+				}
+			});
+		} else {
+			twit.setBearerToken(localStorage.twitterBearerToken);
+		}
+
+		function getLatestStatus(callback){
+			var params = {
+				'screen_name': secrets.twitter.username,
+				'exclude_replies': true,
+				'count': 1
+			};
+			twit.__call(
+				"statuses_userTimeline",
+				params,
+				function (reply) {
+					callback(reply);
+				},
+				true // for application-only authentication
+			);
+		}
+
+		return {
+			getLatestStatus: getLatestStatus
+		};
+	});
+
+	angular.module('main').service('$data', function(TwitterService) {
 		var data = {};
 
 		data.items = [
