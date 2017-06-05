7c98ee157ce4aefc541d228047243bcafe777420
Scott Walters <me@scowalt.com>
2017-06-04 21:51:09 -0700

    Removing plugin files from version control

--- www/js/secrets.js
+++ www/js/secrets.js
@@ -2,14 +2,14 @@
 	'use strict';
 
 	window.secrets = {
-		'gcmProjectNumber': '92241321281',
+		'gcmProjectNumber': '318962663862',
 		'hitbox': {
 			'username': 'newLEGACYinc'
 		},
         'instagram': {
             'username': 'newlegacygram'
         },
-		'serverUrl': 'https://newlegacyinc.me',
+		'serverUrl': 'https://newlegacyinc-app-server-dev.herokuapp.com',
 		'twitter': {
 			'username': 'newLEGACYinc'
 		},
